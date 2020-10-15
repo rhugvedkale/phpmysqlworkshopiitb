dca5095a12d933bbbbc85c6171fab12f7d03e3fd
rhugvedkale <72149714+rhugvedkale@users.noreply.github.com>
2020-10-15 23:44:23 +0530

    day 6 and 7

---
+++ phpmysqlworkshopiitb/day6,7/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+    .container {
+    height: 40vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+    button{
+        height: 40px;
+        width: 120px;
+        margin: 10px;
+    }
+    </style>
+</head>
+<body>
+    <h1 style="text-align: center;">Student Exam Report</h1><br>
+    <div class="container"><img src="https://i.pinimg.com/originals/d4/d7/82/d4d782d5b6e58e1b126094ac1f4dff37.jpg" width="300px" height="300px"></div>
+    <div class="container">
+    <a href="login.php"><button>Login</button></a>
+    <a href="register.php"><button>Register</button></a>
+    <a href="adminlogin.php"><button>Admin Login</button></a>
+    </div>
+</body>
+</html>
